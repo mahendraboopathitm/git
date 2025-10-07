bf7a1b8233fe3d3541ada2bd11524fa7e2815e0a
karancodex <jaikarannatarajan@gmail.com>
2025-10-07 15:02:15 +0530

    print statement changed

--- codes.py
+++ codes.py
@@ -1,11 +1,11 @@
 num=int(input())
 if num<=1:
-    print("not an prime")
+    print("not an prime num")
 else:
     for i in range(2,(num**0.5)+1):
         if num%i==0:
-            print("not an prime")
+            print("not an prime num")
             break
     else:
-        print("prime")
+        print("prime num")
             
